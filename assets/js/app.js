9479a09302e8a278e028c8480630412af5546046
unknown <haupk2003@gmail.com>
2022-02-26 15:41:55 +0700

    one

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$('.customer-list').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    prevArrow:`<button type='button' class='slick-prev pull-left'><img srcset="./assets/img/Vector-prev.png 2x"></button>`,
+    nextArrow:`<button type='button' class='slick-next pull-right'><img srcset="./assets/img/Vector-next.png 2x"></button>`
+  });
+
